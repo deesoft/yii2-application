25142dccf8a56a19faaa868c8f941ac4b8927070
mdmunir <misbahuldmunir@gmail.com>
2015-07-05 23:09:56 +0700

    Enable pretty url

--- environments/dev/app/config/web-local.php
+++ environments/dev/app/config/web-local.php
@@ -5,6 +5,13 @@ $config = [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
         ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => true,
+            'rules' => [
+                'page/<view:\w+>'=>'site/page',
+            ],
+        ],
     ],
 ];
 
--- environments/prod/app/config/web-local.php
+++ environments/prod/app/config/web-local.php
@@ -5,5 +5,12 @@ return [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
         ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                'page/<view:\w+>'=>'site/page',
+            ],
+        ],
     ],
 ];
