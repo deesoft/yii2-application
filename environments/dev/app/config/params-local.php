24ced866d9036438f4c37b93a0f2f66914a941ad
mdmunir <misbahuldmunir@gmail.com>
2015-09-06 10:19:56 +0700

    setup application environment

--- environments/dev/app/config/params-local.php
+++ environments/dev/app/config/params-local.php
@@ -1,4 +1,15 @@
 <?php
 return [
     'rest.baseUrl' => 'application/rest/web/index.php/',
+    'dee.migration.path' => [
+        '@mdm/admin/migrations',
+        '@mdm/autonumber/migrations',
+    ],
+    'migration.rbac' => [
+//        'itemTable' => '{{%auth_item}}',
+//        'itemChildTable' => '{{%auth_item_child}}',
+//        'assignmentTable' => '{{%auth_assignment}}',
+//        'ruleTable' => '{{%auth_rule}}',
+//        'db' => 'db'
+    ],
 ];
